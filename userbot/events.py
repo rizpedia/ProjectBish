d8fbe799d11b9a43cf47bd830247588c0ac67bd2
rizpedia <achmadrizkiramadhan0101@gmail.com>
2020-07-10 21:04:03 +0700

    tes

--- userbot/events.py
+++ userbot/events.py
@@ -139,11 +139,6 @@ def register(**args):
                     file.close()
 
                     if LOGSPAMMER:
-                           await check.client.send_file(send_to,
-                                                        "error.log",
-                                                        caption=text)
-                           remove("error.log")
-                           =======
                         await check.client.respond(
                             "`Sorry, my userbot has crashed."
                             "\nThe error logs are stored in the userbot's log chat.`"
@@ -153,7 +148,6 @@ def register(**args):
                                                  "error.log",
                                                  caption=text)
                     remove("error.log")
->>>>>>> d4cf55839cf2360fa774dce32b95a833a8d08ff8
             else:
                 pass
 
