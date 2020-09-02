facf4316249d4ee634cb164810dc66d00ff95686
rizpedia <you@example.com>
2020-09-02 16:53:41 +0700

    delete some file & add brotli, simg2img, sdat2img.

--- config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$link = "http://ld-bitvpn-webserver-481332435.us-west-1.elb.amazonaws.com:8080/bitproxy/proxy/adReward?deviceId=And.31dc29588cc09697e49b42662c2ad34a.bitvpn&userId=426613198311382&token=1966ae2a45902415d19d766da9495bb5&TrackCode=10220879703273478&countryCode=62&adtype=110&adflag=0&amount=1.600000&orderid=00018400-a024-4fd6-0000-0173588c86e6&sign=&tz=GMT%2b07%3a00&timestamp=1594918471399&siteId=6 HTTP/1.1";
-
