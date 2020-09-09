d6dd344f1ea054abdd87892defd0a7df4ffaa294
rizpedia <44054986+rizpedia@users.noreply.github.com>
2020-09-09 12:23:07 +0700

    Fix caping.py cok

--- userbot/modules/caping.py
+++ userbot/modules/caping.py
@@ -14,18 +14,18 @@ AUTH_CAPING = os.environ.get("AUTH_CAPING", None)
 INDEX_CAPING = os.environ.get("INDEX_CAPING", None)
 TS_CAPING = os.environ.get("TS_CAPING", None)
 
-#url host
+ host
 url = 'https://ai.caping.co.id/v2/event/report'
 
 
 #data json
-data = '{ "reports": [ { "action": "watch_video", "list": [ { "articleType": 512, "newsId":5105951, "status": 0, "times": 2, "totalms": 5 }, { "articleType": 512, "newsId":5105951, "status": 0, "times": 1, "totalms": 0 }, { "articleType": 512, "newsId":, "status": 1, "times": 2, "totalms": 39 } ] } ] }'
+data = '{ "reports": [ { "action": "watch_video", "list": [ { "articleType": 512, "newsId":7609201, "status": 0, "times": 2, "totalms": 5 }, { "articleType": 512, "newsId":7609201, "status": 0, "times": 1, "totalms": 0 }, { "articleType": 512, "newsId":"7609201", "status": 1, "times": 2, "totalms": 39 } ] } ] }'
 
 
 #headers
-headers = {"Accept": "application/json", "Accept-Language": "in", "NETWORKSTATE": "FouthG", "User-Agent": "Mozilla/5.0 (Linux; Android 9; Redmi 6A Build/PQ3B.190801.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36;CapingNews/5.3.0", "Cookie": (COOKIE_CAPING), "Market": "googleplay", "AppId": "1", "loginType": "1", "Authorization": (AUTH_CAPING), "ts": (TS_CAPING), "index" : (INDEX_CAPING), "Content-Type": "application/json", "Connection": "keep-alive"}
+headers = { "Accept": "application/json", "Accept-Language": "in", "NETWORKSTATE": "FouthG", "User-Agent": "Mozilla/5.0 (Linux; Android 9; Redmi 6A Build/PQ3B.190801.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36;CapingNews/5.3.0", "Cookie": (COOKIE_CAPING), "Market": "googleplay", "AppId": "1", "loginType": "1", "Authorization": (AUTH_CAPING), "ts": (TS_CAPING), "index" : (INDEX_CAPING), "Content-Type": "application/json", "Connection": "Keep-Alive" }
 
 
-respon = requests.post(url, json = data, headers = headers)
+respon = requests.post(url = url, headers = headers, data = data)
 
-print(respon)
+print(respon.json())
