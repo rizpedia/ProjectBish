7daad7c152bd3eab979605b25f8011b1b756ee8c
rizpedia <44054986+rizpedia@users.noreply.github.com>
2020-09-09 10:38:27 +0700

    Fix caping.py 2

--- userbot/modules/caping.py
+++ userbot/modules/caping.py
@@ -14,7 +14,7 @@ data = '{ "reports": [ { "action": "watch_video", "list": [ { "articleType": 512
 
 
 #headers
-headers = '{"Accept": "application/json", "Accept-Language": "in", "NETWORKSTATE": "FouthG", "User-Agent": "Mozilla/5.0 (Linux; Android 9; Redmi 6A Build/PQ3B.190801.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36;CapingNews/5.3.0", "Cookie": (COOKIE_CAPING), "Market": "googleplay", "AppId": "1", "loginType": "1", "Authorization": (AUTH_CAPING), "ts": (TS_CAPING), "index" : (INDEX_CAPING), "Content-Type": "application/json", "Connection": "keep-alive"}'
+headers = {"Accept": "application/json", "Accept-Language": "in", "NETWORKSTATE": "FouthG", "User-Agent": "Mozilla/5.0 (Linux; Android 9; Redmi 6A Build/PQ3B.190801.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36;CapingNews/5.3.0", "Cookie": (COOKIE_CAPING), "Market": "googleplay", "AppId": "1", "loginType": "1", "Authorization": (AUTH_CAPING), "ts": (TS_CAPING), "index" : (INDEX_CAPING), "Content-Type": "application/json", "Connection": "keep-alive"}
 
 
 respon = requests.post(url, json = data, headers = headers)
