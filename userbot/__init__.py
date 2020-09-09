92f9610d98c106b63a1a76c2e9823763b21746b9
rizpedia <44054986+rizpedia@users.noreply.github.com>
2020-09-09 10:11:39 +0700

    Add Var For Tuyul Caping.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -53,6 +53,11 @@ if CONFIG_CHECK:
 API_KEY = os.environ.get("API_KEY", None)
 API_HASH = os.environ.get("API_HASH", None)
 
+# Tuyul Caping
+COOKIE_CAPING = os.environ.get("COOKIE_CAPING", None)
+AUTH_CAPING = os.environ.get("AUTH_CAPING", None)
+INDEX_CAPING = os.environ.get("INDEX_CAPING", None)
+TS_CAPING = os.environ.get("TS_CAPING", None)
 
 # Userbot Session String
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
