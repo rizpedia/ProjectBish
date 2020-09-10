dadf3f0ee2ee0014285ad290be6eed69eeaf3960
rizpedia <44054986+rizpedia@users.noreply.github.com>
2020-09-10 13:24:05 +0700

    Update tylcaping.py

--- userbot/modules/tylcaping.py
+++ userbot/modules/tylcaping.py
@@ -29,12 +29,13 @@ async def pingme(pong):
    sesi = 0
    url = 'https://ai.caping.co.id/v2/event/report'
    headers = { "Accept": "application/json", "Accept-Language": "in", "NETWORKSTATE": "FouthG", "User-Agent": "Mozilla/5.0 (Linux; Android 9; Redmi 6A Build/PQ3B.190801.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36;CapingNews/5.3.0", "Cookie": (COOKIE_CAPING), "Market": "googleplay", "AppId": "1", "loginType": "1", "Authorization": (AUTH_CAPING), "ts": (TS_CAPING), "index" : (INDEX_CAPING), "Content-Type": "application/json", "Connection": "Keep-Alive" }
-   id = 7609200
+   id = 7609234
    json = {"reports":[{"action":"browse_news","list":[{"articleType":1,"newsId":(id),"status":1,"times":3,"totalms":40},{"articleType":1,"newsId":(id),"status":1,"times":2,"totalms":37}]}]}
    datajson = "=======Nuyul Berita======="
    while sesi < 20 :
         time.sleep(1)
         id = id + 1
+        json = {"reports":[{"action":"browse_news","list":[{"articleType":1,"newsId":(id),"status":1,"times":3,"totalms":40},{"articleType":1,"newsId":(id),"status":1,"times":2,"totalms":37}]}]}
         respon = requests.post(url = url, headers = headers, json = json)
         responjson = (respon.json())
         sesi = sesi + 1
