a20cbe3196d2546a5e45821e0e4d8085e7ba0884
rizpedia <44054986+rizpedia@users.noreply.github.com>
2020-09-10 07:07:57 +0700

    Add caping berita

--- userbot/modules/tylcaping.py
+++ userbot/modules/tylcaping.py
@@ -19,9 +19,27 @@ INDEX_CAPING = os.environ.get("INDEX_CAPING", None)
 TS_CAPING = os.environ.get("TS_CAPING", None)
 
 
-@register(outgoing=True, pattern="^.caping$")
+@register(outgoing=True, pattern="^.capingb$")
 async def pingme(pong):
-   await pong.edit("Tuyul dimulai")
+   await pong.edit("Tuyul berita dimulai")
+   sesi = 0
+   url = 'https://ai.caping.co.id/v2/event/report'
+   headers = { "Accept": "application/json", "Accept-Language": "in", "NETWORKSTATE": "FouthG", "User-Agent": "Mozilla/5.0 (Linux; Android 9; Redmi 6A Build/PQ3B.190801.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36;CapingNews/5.3.0", "Cookie": (COOKIE_CAPING), "Market": "googleplay", "AppId": "1", "loginType": "1", "Authorization": (AUTH_CAPING), "ts": (TS_CAPING), "index" : (INDEX_CAPING), "Content-Type": "application/json", "Connection": "Keep-Alive" }
+   id = 5105951
+   json = { "reports": [ { "action": "watch_video", "list": [ { "articleType": 512, "newsId":(id), "status": 0, "times": 2, "totalms": 5 }, { "articleType": 512, "newsId":(id), "status": 1, "times": 2, "totalms": 39 } ] } ] }
+   datajson = "=======Nuyul Berita======="
+   while sesi < 20 :
+        time.sleep(1)
+        id = id - 1
+        respon = requests.post(url = url, headers = headers, json = json)
+        responjson = (respon.json())
+        sesi = sesi + 1
+        datajson = datajson + "\n" + (str(responjson))
+        await pong.edit(str(datajson))
+
+@register(outgoing=True, pattern="^.capingv$")
+async def pingme(pong):
+   await pong.edit("Tuyul video dimulai")
    sesi = 0
    url = 'https://ai.caping.co.id/v2/event/report'
    headers = { "Accept": "application/json", "Accept-Language": "in", "NETWORKSTATE": "FouthG", "User-Agent": "Mozilla/5.0 (Linux; Android 9; Redmi 6A Build/PQ3B.190801.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36;CapingNews/5.3.0", "Cookie": (COOKIE_CAPING), "Market": "googleplay", "AppId": "1", "loginType": "1", "Authorization": (AUTH_CAPING), "ts": (TS_CAPING), "index" : (INDEX_CAPING), "Content-Type": "application/json", "Connection": "Keep-Alive" }
@@ -38,6 +56,8 @@ async def pingme(pong):
         await pong.edit(str(datajson))
 CMD_HELP.update({
      "caping":
-     ">.caping"
-     "\nUsage: Untuk nuyul caping, jika data 0 berarti kena limit harian."
+     ">.capingv"
+     "\nUsage: Untuk nuyul video caping, jika data 0 berarti kena limit harian."
+     ">.capingb"
+     "\nUsage: Untuk nuyul berita caping, jika data 0 berarti kena limit harian."
 })
