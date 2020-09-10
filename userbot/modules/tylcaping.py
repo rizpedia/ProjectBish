507eb0431b5c9bc70682052244d4eedf5c3dd131
rizpedia <44054986+rizpedia@users.noreply.github.com>
2020-09-10 12:10:30 +0700

    Tes

--- userbot/modules/tylcaping.py
+++ userbot/modules/tylcaping.py
@@ -25,16 +25,17 @@ async def pingme(pong):
    sesi = 0
    url = 'https://ai.caping.co.id/v2/event/report'
    headers = { "Accept": "application/json", "Accept-Language": "in", "NETWORKSTATE": "FouthG", "User-Agent": "Mozilla/5.0 (Linux; Android 9; Redmi 6A Build/PQ3B.190801.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36;CapingNews/5.3.0", "Cookie": (COOKIE_CAPING), "Market": "googleplay", "AppId": "1", "loginType": "1", "Authorization": (AUTH_CAPING), "ts": (TS_CAPING), "index" : (INDEX_CAPING), "Content-Type": "application/json", "Connection": "Keep-Alive" }
-   id = 5105951
-   json = { "reports": [ { "action": "watch_video", "list": [ { "articleType": 512, "newsId":(id), "status": 0, "times": 2, "totalms": 5 }, { "articleType": 512, "newsId":(id), "status": 1, "times": 2, "totalms": 39 } ] } ] }
-   datajson = "=======Nuyul Berita======="
-   while sesi < 20 :
-        time.sleep(1)
-        id = id - 1
+   id = 7609201
+   json = {"reports":[{"action":"browse_news","list":[{"articleType":1,"newsId":(id),"status":1,"times":3,"totalms":40},{"articleType":1,"newsId":(id),"status":1,"times":2,"totalms":37}]}]}
+   datajson = "======Nuyul Video======"
+   while sesi < 4 :
+        time.sleep(2)
+        id = id + 1
         respon = requests.post(url = url, headers = headers, json = json)
         responjson = (respon.json())
         sesi = sesi + 1
         datajson = datajson + "\n" + (str(responjson))
+        print(datajson)
         await pong.edit(str(datajson))
 
 @register(outgoing=True, pattern="^.capingv$")
