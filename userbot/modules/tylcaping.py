aef4220a3acfd70a2f05f621773b109bc54685fc
rizpedia <44054986+rizpedia@users.noreply.github.com>
2020-09-11 05:49:29 +0700

    Update tylcaping.py

--- userbot/modules/tylcaping.py
+++ userbot/modules/tylcaping.py
@@ -29,10 +29,10 @@ async def pingme(pong):
    sesi = 0
    url = 'https://ai.caping.co.id/v2/event/report'
    headers = { "Accept": "application/json", "Accept-Language": "in", "NETWORKSTATE": "FouthG", "User-Agent": "Mozilla/5.0 (Linux; Android 9; Redmi 6A Build/PQ3B.190801.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36;CapingNews/5.3.0", "Cookie": (COOKIE_CAPING), "Market": "googleplay", "AppId": "1", "loginType": "1", "Authorization": (AUTH_CAPING), "ts": (TS_CAPING), "index" : (INDEX_CAPING), "Content-Type": "application/json", "Connection": "Keep-Alive" }
-   id = 7609225
+   id = 7609201
    json = {"reports":[{"action":"browse_news","list":[{"articleType":1,"newsId":(id),"status":1,"times":3,"totalms":40},{"articleType":1,"newsId":(id),"status":1,"times":2,"totalms":37}]}]}
    datajson = "=======Nuyul Berita======="
-   while sesi < 55 :
+   while sesi < 60 :
         time.sleep(1)
         id = id + 1
         json = {"reports":[{"action":"browse_news","list":[{"articleType":1,"newsId":(id),"status":1,"times":3,"totalms":40},{"articleType":1,"newsId":(id),"status":1,"times":2,"totalms":37}]}]}
@@ -44,7 +44,7 @@ async def pingme(pong):
 CMD_HELP.update({
      "caping":
      ">.capingv"
-     "\nUsage: Untuk nuyul video caping, jika data 0 berarti kena limit harian."
+     "\nUsage: Untuk nuyul video caping, jika data 0 berarti kena limit harian.\n"
      ">.capingb"
      "\nUsage: Untuk nuyul berita caping, jika data 0 berarti kena limit harian."
 })
