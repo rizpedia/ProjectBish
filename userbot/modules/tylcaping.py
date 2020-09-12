1ee6fabf61375f538b85933582f6c07d13f26809
rizpedia <44054986+rizpedia@users.noreply.github.com>
2020-09-12 14:12:33 +0700

    Try to fix
    Dah belajar while if else :v

--- userbot/modules/tylcaping.py
+++ userbot/modules/tylcaping.py
@@ -26,21 +26,25 @@ INDEX_CAPING = '39'
 @register(outgoing=True, pattern="^.capingb$")
 async def pingme(pong):
    await pong.edit("Tuyul berita dimulai")
-   sesi = 0
    url = 'https://ai.caping.co.id/v2/event/report'
    headers = { "Accept": "application/json", "Accept-Language": "in", "NETWORKSTATE": "FouthG", "User-Agent": "Mozilla/5.0 (Linux; Android 9; Redmi 6A Build/PQ3B.190801.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36;CapingNews/5.3.0", "Cookie": (COOKIE_CAPING), "Market": "googleplay", "AppId": "1", "loginType": "1", "Authorization": (AUTH_CAPING), "ts": (TS_CAPING), "index" : (INDEX_CAPING), "Content-Type": "application/json", "Connection": "Keep-Alive" }
    id = 7609201
    json = {"reports":[{"action":"browse_news","list":[{"articleType":1,"newsId":(id),"status":1,"times":3,"totalms":40},{"articleType":1,"newsId":(id),"status":1,"times":2,"totalms":37}]}]}
    datajson = "=======Nuyul Berita======="
-   while sesi < 80 :
+   start = True
+   sesi = 0
+   while start:
         time.sleep(1)
         id = id + 1
         json = {"reports":[{"action":"browse_news","list":[{"articleType":1,"newsId":(id),"status":1,"times":3,"totalms":40},{"articleType":1,"newsId":(id),"status":1,"times":2,"totalms":37}]}]}
         respon = requests.post(url = url, headers = headers, json = json)
         responjson = (respon.json())
-        sesi = sesi + 1
         datajson = datajson + "\n" + (str(responjson))
-        await pong.edit(str(datajson))
+                if sesi == 2:
+                   await pong.edit(str(datajson))
+                   start = False
+        sesi += 1
+
 CMD_HELP.update({
      "caping":
      ">.capingv"
